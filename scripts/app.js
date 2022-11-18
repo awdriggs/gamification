0dd6417784c0a5b41534e953458576aae51a2e10
Adam Driggers <awdriggs@gmail.com>
2022-11-17 19:54:49 -0500

    pushing changes

--- scripts/app.js
+++ scripts/app.js
@@ -13,3 +13,26 @@ function update(){
   //add a badge every 100 points
 }
 
+
+
+// video shit - referenced...https://forum.webdeveloper.com/d/361855-how-to-best-track-how-long-a-video-has-been-played
+
+// get html video player
+// var video_data = document.getElementById("vid");
+
+// callback function
+// function videoStartedPlaying(){
+//     console.log("play");
+// }
+
+//event listener
+// video_data.addEventListener("play", videoStartedPlaying);
+
+//create a js timeout that start give points for each x seconds watched
+
+//pause event listener to stop the timeout
+// video_data.addEventListener("pause", videoStoppedPlaying);
+// video_data.addEventListener("playing", videoStartedPlaying); //how does this work?
+// video_data.addEventListener("ended", videoStoppedPlaying); //give bonus points for finishing the video?
+
+ 
