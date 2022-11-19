3f2ebe98c61dc5b243cf663c6a6abb9144d5e5b8
Adam Driggers <awdriggs@gmail.com>
2022-11-19 15:05:36 -0500

    badges, modals, data

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,55 @@
 console.log("working bro");
+//variables for keeping track of stuff
+let points = 0; //plant a cookie to keep track of points?
+let nextBadgeIndex = 0;
 
-let points = 0;
-
-//grab the points span from dom
+//dom elements
+//grab the points span from dom and store in a variable
 let pointsSpan = document.querySelector('#points');
 
-//you'll need to update the points alot,  
+//functions
+//you'll need to update the points alot,
 function update(){
   pointsSpan.innerHTML = points;
-  
+
   //add a badge?
   //add a badge every 100 points
+  if(points > 0 && points <= 800 && points % 100 == 0){
+    alert("you've unlocked a new badge!"); //use a modal instead?
+    //get badge info from data.js 
+    let badgeData = achievements[nextBadgeIndex];
+    //launch modal with data
+    launchModal(badgeData);
+
+
+    //add badge to footer
+    let foot = document.querySelector('footer'); //append image to the footer
+    let badge = document.createElement('img'); //create a new image
+    badge.setAttribute('src', badgeData.src); //set image source attribute, concatenating the string name for the image
+    
+    // badge.setAttribute('src', '../badges/badges' + nextBadgeIndex + '.png'); //set image source attribute, concatenating the string name for the image
+    foot.append(badge); //append to parent
+
+    nextBadgeIndex++; //que up the next badge
+  }
+  // no more badges!
+  if(points == 800){
+    alert("You've completed the gamification quest!");
+  }
 }
 
+//see when you have got to the bottom of the screen
 
+// quiz show? how have questions about gamification to answer stored as a json file.
+// get a random value from the questons index
+// use values to populate a form
+// user clicks button to check if their answer is write, points given for each correct answer, 10 points?
+// points taken away for each answer.
+// ex questons = [{question: "what color is sky?", multi: [a: "red", b: "blue", c: "green", d: "black"], ans: c}],
 
-// video shit - referenced...https://forum.webdeveloper.com/d/361855-how-to-best-track-how-long-a-video-has-been-played
+// see if the user is watched the video, give them so points for every x seconds watched
+// give them some points if they finish the video
+// video stuff - referenced...https://forum.webdeveloper.com/d/361855-how-to-best-track-how-long-a-video-has-been-played
 
 // get html video player
 // var video_data = document.getElementById("vid");
@@ -35,4 +69,27 @@ function update(){
 // video_data.addEventListener("playing", videoStartedPlaying); //how does this work?
 // video_data.addEventListener("ended", videoStoppedPlaying); //give bonus points for finishing the video?
 
- 
+// Modal business, w3 schools
+var modal = document.getElementById("badgeModal"); // Get the modal
+var btn = document.getElementById("myBtn"); //button for testing the modal
+var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+
+// When the user clicks on the button, open the modal
+function launchModal(bData) {
+  modal.style.display = "block";
+  // debugger;
+  modal.querySelector('.badge-title').innerHTML = bData.title;
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// // When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
---
+++ scripts/data.js
@@ -0,0 +1,14 @@
+let test = "some nonsense";
+
+// Bagdes json
+// an array of objects,
+// each obj has a title, src for img url, and an description
+let achievements = [
+  {title: "First Badge Unlocked!", src: "../badges/trophy.png", desc: "Congratulations! You've unlocked your first achievement badge. Get another 100 points to get your next badge."},
+  {title: "Blast Off!!", src: "../badges/shuttle.png", desc: "Wow! You're really taking off. Keep going for more badges!"}, 
+  
+]
+
+// questions
+// an array of objects
+// ex questons = [{question: "what color is sky?", multi: [a: "red", b: "blue", c: "green", d: "black"], ans: c}],
